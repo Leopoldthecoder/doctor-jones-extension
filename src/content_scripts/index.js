e620b4d3f99377477ee5e57af89370957c24416d
Leopoldthecoder <Leopoldthecuber@gmail.com>
2019-07-05 22:15:25 +0800

    feat: auto format

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,8 +1,21 @@
 import dj from "doctor-jones";
 import { messageType } from "../const";
+import { debounce } from "debounce";
+
+let storedOptions = {};
 
 let textNodes = [];
 let touched = false;
+
+let execStart;
+let execCount = 0;
+let execFlag = true;
+
+chrome.storage.sync.get(["FORMAT_OPTIONS"], result => {
+  if (result && result["FORMAT_OPTIONS"]) {
+    storedOptions = result["FORMAT_OPTIONS"];
+  }
+});
 
 const walk = nodes => {
   nodes.forEach(node => {
@@ -14,6 +27,65 @@ const walk = nodes => {
   });
 };
 
+const pauseExec = () => {
+  execFlag = false;
+  setTimeout(() => {
+    execFlag = true;
+    execStart = null;
+    execCount = 0;
+  }, 3000);
+};
+
+const format = (options, mustExec = false) => {
+  if (!mustExec) {
+    if (!execFlag) return;
+    if (execStart && Date.now() - execStart <= 3000) {
+      execCount++;
+      if (execCount >= 20) {
+        // 若三秒内执行 20 次以上 format，则暂停三秒
+        pauseExec();
+      }
+    } else {
+      execStart = Date.now();
+    }
+  }
+
+  touched = true;
+  textNodes = [];
+  walk([document.documentElement]);
+  textNodes.forEach(node => {
+    if (node.hasChildNodes()) return;
+    switch (node.nodeType) {
+      case 1:
+        node._innerText = node._innerText || node.innerText;
+        node.innerText = dj(node.innerText, options);
+        break;
+      case 3:
+        node._nodeValue = node._nodeValue || node.nodeValue;
+        node.nodeValue = dj(node.nodeValue, options);
+        break;
+      default:
+        break;
+    }
+  });
+};
+
+const debouncedFormat = debounce(format, 100);
+
+const mutationHandler = mutationList => {
+  mutationList.forEach(() => {
+    debouncedFormat(storedOptions);
+  });
+};
+
+const observerOptions = {
+  childList: true,
+  subtree: true
+};
+
+const observer = new MutationObserver(mutationHandler);
+observer.observe(document.body, observerOptions);
+
 chrome.runtime.onMessage.addListener((request, sender) => {
   if (chrome.runtime.id !== sender.id) {
     return;
@@ -21,24 +93,7 @@ chrome.runtime.onMessage.addListener((request, sender) => {
 
   switch (request.type) {
     case messageType.format:
-      touched = true;
-      textNodes = [];
-      walk([document.documentElement]);
-      textNodes.forEach(node => {
-        if (node.hasChildNodes()) return;
-        switch (node.nodeType) {
-          case 1:
-            node._innerText = node._innerText || node.innerText;
-            node.innerText = dj(node.innerText, request.options);
-            break;
-          case 3:
-            node._nodeValue = node._nodeValue || node.nodeValue;
-            node.nodeValue = dj(node.nodeValue, request.options);
-            break;
-          default:
-            break;
-        }
-      });
+      format(request.options, true);
       break;
 
     case messageType.revoke:
